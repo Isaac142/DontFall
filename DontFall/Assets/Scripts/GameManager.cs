26ba20da8dfe260d01da9e66f90cce5d4f76484e
Isaac <embramaze@gmail.com>
2020-02-17 19:14:39 +1100

    Timer

---
+++ DontFall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float timer;
+
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        int seconds = (int)(timer % 60);
+        int minutes = (int)(timer / 60) % 60;
+
+        string timerString = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        timeText.text = timerString;
+
+        if (timer <= 30)
+        {
+            timeText.text = "Time Left: " + timer.ToString("F2");
+        }
+        else
+        {
+            timeText.text = "Time Left: " + timerString;
+        }
+
+        if (timer <= 0)
+        {
+            timer = 0f;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+            Time.timeScale = 1f;
+        }
+    }
+}
