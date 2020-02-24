1ffcc38272777a4e92e519af59afa5a57928933c
Isaac <embramaze@gmail.com>
2020-02-24 22:12:19 +1100

    UI

--- DontFall/Assets/Scripts/GameManager.cs
+++ DontFall/Assets/Scripts/GameManager.cs
@@ -10,6 +10,22 @@ public class GameManager : MonoBehaviour
 
     public Text timeText;
 
+    public bool isPaused = false;
+    public GameObject pausePanel;
+
+    public bool isTitleScreen;
+    public bool wonGame;
+
+    public GameObject youWin;
+    public GameObject youLost;
+    public GameObject inGameHUD;
+
+    public Respawn RP;
+
+    public GameObject player;
+
+    public PlayerCameraMovement PCMX, PCMY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +34,6 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
-
-    private void FixedUpdate()
     {
         int seconds = (int)(timer % 60);
         int minutes = (int)(timer / 60) % 60;
@@ -40,15 +51,55 @@ public class GameManager : MonoBehaviour
             timeText.text = "Time Left: " + timerString;
         }
 
+        if (!isTitleScreen)
+        {
+            timeText.text = "Time Left: " + timerString;
+        }
+
         if (timer <= 0)
         {
             timer = 0f;
             Time.timeScale = 0f;
+            PCMX.canMove = false;
+            PCMY.canMove = false;
+            youLost.SetActive(true);
+            inGameHUD.SetActive(false);
         }
-        else
+        else if (!isPaused && !wonGame)
         {
+            Debug.Log("Not Paused");
             timer -= Time.deltaTime;
             Time.timeScale = 1f;
+            PCMX.canMove = true;
+            PCMY.canMove = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            Pause(isPaused);
+            if (isPaused)
+            {
+                Time.timeScale = 0f;
+                PCMX.canMove = false;
+                PCMY.canMove = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    public void Pause(bool pause)
+    {
+
+        if (pausePanel != null)
+        {
+            pausePanel.SetActive(pause);
+            inGameHUD.SetActive(!pause);
+
         }
     }
 }
--- DontFall/Assets/Scripts/Respawn.cs
+++ DontFall/Assets/Scripts/Respawn.cs
@@ -7,6 +7,10 @@ public class Respawn : MonoBehaviour
 {
     public GameObject player;
 
+    public GameManager GM;
+
+    public PlayerCameraMovement PCMX, PCMY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +31,25 @@ public class Respawn : MonoBehaviour
         if (other.gameObject.CompareTag("Red_Wall"))
         {
             Debug.Log("hit");
+            GM.youLost.SetActive(true);
+            Time.timeScale = 0f;
+            GM.inGameHUD.SetActive(false);
+            PCMX.canMove = false;
+            PCMY.canMove = false;
             //player.GetComponent<Renderer>().material.color = Color.green;
-            SceneManager.LoadScene("_Scene_01");
+            //SceneManager.LoadScene("_Scene_01");
+        }
+
+        if (other.gameObject.CompareTag("Win_Wall"))
+        {
+            Debug.Log("hit");
+            GM.youWin.SetActive(true);
+            Time.timeScale = 0f;
+            GM.inGameHUD.SetActive(false);
+            PCMX.canMove = false;
+            PCMY.canMove = false;
+            //player.GetComponent<Renderer>().material.color = Color.green;
+            //SceneManager.LoadScene("_Scene_01");
         }
     }
 }
